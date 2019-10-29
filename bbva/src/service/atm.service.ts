f60d42093e3e752552f1cbbaea9993f699c5f377
Luis Romero <larzrs@gmail.com>
2019-10-29 11:00:25 -0600

    Agregando servicio

--- bbva/src/service/atm.service.ts
+++ bbva/src/service/atm.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AtmService {
+  url: string = "https://apis.bbvabancomer.com/locations_sbx/v1/atms";
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  obtenerATM() {
+    return this.http.get(this.url);
+  }
 }
