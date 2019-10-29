56f5d10b504a6b0c2b8206cedd2383b1ec524b6a
ejcv <ejcv.123@gmail.com>
2019-10-29 11:55:14 -0600

    Se creo el componente de Sucursal

--- bbva/src/app/sucursal/sucursal.component.ts
+++ bbva/src/app/sucursal/sucursal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SucService} from 'src/service/suc.service';
 
 @Component({
   selector: 'app-sucursal',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SucursalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private sucService: SucService
+  ) { }
 
   ngOnInit() {
+    console.log("Funciona");
+
+    this.sucService.obtenerSucursal()
+    .subscribe(
+      data => {
+        console.log(data);
+      }
+    );
   }
 
 }
