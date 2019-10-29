fa91e5e3a41c446ffbf9850923dfe8283ea04490
Luis Romero <larzrs@gmail.com>
2019-10-29 11:04:56 -0600

    Agregando rutas

--- bbva/src/app/app-routing.module.ts
+++ bbva/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AtmComponent } from './atm/atm.component';
+import { SucursalComponent } from './sucursal/sucursal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'atm', component: AtmComponent},
+  {path: 'suc', component: SucursalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
