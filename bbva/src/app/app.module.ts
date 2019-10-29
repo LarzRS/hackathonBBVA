fe1283b29fe24096a010904de118d753fc603b24
Luis Romero <larzrs@gmail.com>
2019-10-29 10:59:56 -0600

    agregando paginas

--- bbva/src/app/app.module.ts
+++ bbva/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AtmComponent } from './atm/atm.component';
+import { SucursalComponent } from './sucursal/sucursal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AtmComponent,
+    SucursalComponent
   ],
   imports: [
     BrowserModule,
